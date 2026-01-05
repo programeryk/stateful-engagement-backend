0e994acbaec807d4c63ae90b34fe95001e5a871e
atmosferyk <programeryk@gmail.com>
2026-01-05 19:55:22 +0100

    fixed main.ts to load .env across teh entire app. Hooked up actual database entries on entities.service and me.service.

--- src/entities/entities.service.ts
+++ src/entities/entities.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EntitiesService {
-  getMeState(userId: string) {
-    return {
-      userId,
-      energy: 50,
-      loyalty: 0,
-      fatigue: 0,
-      level: 1,
-    };
+  constructor(private prisma: PrismaService) {}
+
+  async getMeState(userId: string) {
+    const entity = await this.prisma.entity.findUnique({
+      where: { userId },
+      include: { state: true },
+    });
+
+    if (!entity) {
+      throw new NotFoundException('entity not found');
+    }
+    if (!entity.state) {
+      throw new NotFoundException('state not found');
+    }
+
+    return entity.state;
   }
 }
--- src/me/me.service.ts
+++ src/me/me.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class MeService {
-  getMe(userId: string) {
+  constructor(private prisma: PrismaService) {}
+
+  async getMe(userId: string) {
+    const user = await this.prisma.user.upsert({
+      where: { id: userId },
+      update: {},
+      create: { id: userId },
+    });
+    const entity = await this.prisma.entity.upsert({
+      where: { userId },
+      update: {},
+      create: { userId },
+    });
+    const state = await this.prisma.entityState.upsert({
+      where: { entityId: entity.id },
+      update: {},
+      create: { entityId: entity.id },
+    });
+
     return {
-      id: userId,
-      email: 'mock@local',
+      user,
+      entity,
+      state,
     };
   }
 }
