dbbaec3e5cd0f7d236b9a3c1c79e41427888b326
ezoteryk <programeryk@gmail.com>
2026-01-22 18:18:31 +0100

    feat: add tool related routing
    -/tools
    -/inventory

--- src/tools/tools.controller.ts
+++ src/tools/tools.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { ToolsService } from './tools.service';
+import { UserId } from 'src/common/user-id.decorator';
 
-@Controller('tools')
+@Controller()
 export class ToolsController {
   constructor(private readonly toolsService: ToolsService) {}
-  @Get()
+  @Get('tools')
   getTools() {
     return this.toolsService.getTools();
+  }
+  @Get('inventory')
+  getInventory(@UserId() userId: string) {
+    return this.toolsService.getInventory(userId);
   }
 }
