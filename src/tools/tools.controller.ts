dd73082e1c06dcb76ff9597e255fb5c22364b0c2
ezoteryk <programeryk@gmail.com>
2026-01-29 16:36:57 +0100

    feat: cleaner API design
    -changed the routes for cleaner API design

--- src/tools/tools.controller.ts
+++ src/tools/tools.controller.ts
@@ -2,10 +2,10 @@ import { Controller, Get, Param, Post } from '@nestjs/common';
 import { ToolsService } from './tools.service';
 import { UserId } from 'src/common/user-id.decorator';
 
-@Controller()
+@Controller('tools')
 export class ToolsController {
   constructor(private readonly toolsService: ToolsService) {}
-  @Get('tools')
+  @Get()
   getTools() {
     return this.toolsService.getTools();
   }
