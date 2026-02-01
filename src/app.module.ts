705e9411f6632527eabb02851b435825903061d0
ezoteryk <programeryk@gmail.com>
2026-02-01 15:47:35 +0100

    test: set up test db

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env'], // later: ['.env.test', '.env']
+      envFilePath: ['.env', '.env.test'],
     }),
     PrismaModule,
     UsersModule,
