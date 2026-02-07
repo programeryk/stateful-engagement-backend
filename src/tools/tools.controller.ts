d65b220cc7d0a3025e81e8840ea3cdaa2204c5ec
ezoteryk <programeryk@gmail.com>
2026-02-07 21:59:30 +0100

    refactor: make tools public while guarding other routes

--- src/tools/tools.controller.ts
+++ src/tools/tools.controller.ts
@@ -4,7 +4,6 @@ import { UserId } from 'src/common/user-id.decorator';
 import { JwtGuard } from 'src/auth/jwt.guard';
 import { CurrentUserId } from 'src/common/currentuser-id.decorator';
 
-@UseGuards(JwtGuard)
 @Controller('tools')
 export class ToolsController {
   constructor(private readonly toolsService: ToolsService) {}
@@ -12,14 +11,17 @@ export class ToolsController {
   getTools() {
     return this.toolsService.getTools();
   }
+  @UseGuards(JwtGuard)
   @Get('inventory')
   getInventory(@CurrentUserId() userId: string) {
     return this.toolsService.getInventory(userId);
   }
+  @UseGuards(JwtGuard)
   @Post('inventory/buy/:toolId')
   buyTool(@CurrentUserId() userId: string, @Param('toolId') toolId: string) {
     return this.toolsService.buyTool(userId, toolId);
   }
+  @UseGuards(JwtGuard)
   @Post(':toolId/use')
   useTool(@CurrentUserId() userId: string, @Param('toolId') toolId: string) {
     return this.toolsService.useTool(userId, toolId);
