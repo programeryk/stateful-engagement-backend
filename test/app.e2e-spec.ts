a031305156e25ff6169d9a08b9a82ab67ce1b80c
ezoteryk <programeryk@gmail.com>
2026-02-10 16:59:15 +0100

    refactor: make tiny app landing changes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -24,6 +24,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('This is the stateful engagement system!');
   });
 });
