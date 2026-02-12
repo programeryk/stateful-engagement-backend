91f78491a3db16b44cf449ce5b458d2570e0c897
atmosferyk <programeryk@gmail.com>
2026-02-12 15:32:49 +0100

    feat: return process health

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import packageJson from '../../package.json'
 
 @Injectable()
-export class HealthService {}
+export class HealthService {
+    constructor(private prisma: PrismaService) { }
+
+    async getHealth() {
+        const timestamp = new Date().toISOString();
+        const uptime = Math.floor(process.uptime());
+        const version = packageJson.version;
+        let dbStatus = 'up';
+
+        try {
+            await this.prisma.$queryRaw`SELECT 1`;
+        } catch {
+            dbStatus = 'down'
+        }
+        return {
+            status: dbStatus === 'up' ? 'ok' : 'degraded', timestamp, uptime, version, services: {
+                database: dbStatus
+            }
+        };
+    }
+}
